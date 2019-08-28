76a09df77b918452cd349b6fe59da35b8c80ac02
Ubuntu <ubuntu@ip-172-31-85-103.ec2.internal>
2019-08-28 15:18:05 +0000

    Added heroku and postgres support for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['00ea81ca1dfa4f0f8af31b310070a153.vfs.cloud9.us-east-1.amazonaws.com','blog-test-app-testproject']
+ALLOWED_HOSTS = ['00ea81ca1dfa4f0f8af31b310070a153.vfs.cloud9.us-east-1.amazonaws.com','blog-test-app-testproject.herokuapp.com']
 
 
 # Application definition
